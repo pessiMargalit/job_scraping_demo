f8f62059a05590f65b8fadb8049e29a8462c30ff
ShaniShurkin <shanishurkin@gmail.com>
2023-12-12 15:37:36 +0200

    Fix issues

--- Scrapers/CompanyScrapers/GoarcScraper.py
+++ Scrapers/CompanyScrapers/GoarcScraper.py
@@ -1,34 +1,32 @@
 from Scrapers.Scraper import *
 
-TIMEOUT_IN_SECONDS = 10
+TIMEOUT_IN_SECONDS = 30
 
 
 class GoarcScraper(Scraper):
     url = 'https://go-arc.com/careers/'
     name = 'Goarc'
 
-    def __init__(self):
-        super(GoarcScraper, self).__init__()
-
     def scrape(self):
         driver = self.selenium_url_maker(self.url)
-
         a_tags = driver.find_elements(By.CLASS_NAME, 'comeet-position')
+        WebDriverWait(driver, TIMEOUT_IN_SECONDS).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'comeet-position')))
         for a_tag in a_tags:
-            link = a_tag.get_attribute('href')
-            a_tag.click()
-            WebDriverWait(driver, TIMEOUT_IN_SECONDS).until(EC.url_changes(self.url))
-            new_page_soup = BeautifulSoup(driver.page_source, 'html.parser')
+            job_url = a_tag.get_attribute('href')
+            driver2 = self.selenium_url_maker(job_url)
+            new_page_soup = BeautifulSoup(driver2.page_source, 'html.parser')
             data = new_page_soup.find('div', {'class': 'comeet-outer-wrapper'})
-            title = data.findNext('h2', {'class': 'comeet-position-name'})
-            location = data.findNext('span', {'class': 'comeet-position-location'})
+            title = data.find('h2', {'class': 'comeet-position-name'})
+            location = data.find('span', {'class': 'comeet-position-location'})
             self.positions.append(self.Position(
                 title=title.text if title else None,
-                link=link if link else self.url,
+                link=job_url if job_url else self.url,
                 location=location.text if location else None,
             ))
+            driver2.quit()
 
         driver.quit()
 
 
-GoarcScraper().scrape()
+GoarcScraper().check_self()
