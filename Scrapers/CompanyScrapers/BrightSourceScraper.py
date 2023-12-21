0f9e966cc816b086eac5438da7b38ade7917419e
Noa <nzinger123@gmail.com>
2023-12-21 10:37:48 +0200

    done scraper :)

---
+++ Scrapers/CompanyScrapers/BrightSourceScraper.py
@@ -0,0 +1,25 @@
+
+from Scrapers.Scraper import *
+class BrightSourceScraper(Scraper):
+
+    name = 'BrightSource'
+    url = 'https://www.newbrightsource.com/careers'
+    location = 'west Jerusalem'  # default location, when not set its automatically 'Jerusalem'
+
+    def scrape(self):
+        soup = self.scraping_unit(self.url)
+        for dcaree in soup.findAll('div',{'class':'KcpHeO tz5f0K comp-l10svr3o wixui-rich-text'}):
+            text= dcaree.findNext('span').text
+            text = text.split(",")
+            print(text)
+            title=text[0]
+            location=text[1]
+            link=dcaree.findNext('a')
+        self.positions.append(self.Position(
+            title=title,
+            link=link['href'],
+            location=location
+        ))
+
+
+BrightSourceScraper().check_self()
