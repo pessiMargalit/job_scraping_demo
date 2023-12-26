4b32f5239f1a132af77bcd805e87f402d84216b5
YehuditShrg <y0548562311@outlook.co.il>
2023-12-26 20:39:15 +0200

    Check Point done !!

--- Scrapers/CompanyScrapers/CheckPointScraper.py
+++ Scrapers/CompanyScrapers/CheckPointScraper.py
@@ -1,1 +1,26 @@
+from Scrapers.Scraper import *
 
+
+class CheckPointScraper(Scraper):
+    url = 'https://careers.checkpoint.com/?q=&module=cpcareers&a=search&fa%5B%5D=country_ss%3AIsrael&sort='
+    name = 'CheckPoint'
+
+    def scrape(self):
+        soup = self.scraping_unit(self.url)
+        position_results = soup.find('div', id='positionResults')
+        for job in position_results.findAll('div', class_='position'):
+            title = job.findNext('a').text
+            link = job.findNext('a')['href']
+            info = job.findNext('div', class_='posInfo')
+            location = info.findNext('p').text
+            # the location is in the first p tag, the tag doesn't have any specific identifier.
+            self.positions.append(
+                self.Position(
+                    title=title,
+                    link=link,
+                    location=location
+                )
+            )
+
+
+CheckPointScraper().check_self()
