fb5a3083ec3cdbef41949bfeb2e9cea06e1b978d
ShaniShurkin <shanishurkin@gmail.com>
2023-12-27 15:42:59 +0200

    Etoro scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/EtoroScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.BaseScraper import ComeetScraper
+
+
+class EtoroScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/etoro/41.009"
+    name = "Etoro"
+
+    def scrape(self):
+        super().scrape()
+
+    
