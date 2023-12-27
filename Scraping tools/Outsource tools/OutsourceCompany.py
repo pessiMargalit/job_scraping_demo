f98daa2f1f347b44f1b3da5c4bc2ba8c32c6d1c1
YehuditShrg <y0548562311@outlook.co.il>
2023-12-27 12:48:45 +0200

    base outsource is redeay

---
+++ Scraping tools/Outsource tools/OutsourceCompany.py
@@ -0,0 +1,58 @@
+import re
+import subprocess
+
+
+class OutsourceCompany:
+    def __init__(self, name, url=None):
+        self.name = name
+        self.url = url
+
+    @staticmethod
+    def transform_string(input_string):
+        # Replace special characters with spaces
+        cleaned_string = re.sub(r'[^a-zA-Z0-9\s]', ' ', input_string)
+        capitalized_string = cleaned_string.title()
+        final_string = ''.join(capitalized_string.split())
+        return final_string
+
+    def generate_scraper(self, directory_path=None):
+        pass
+
+    def create_branch(self, branch_name):
+        """
+        This function create a new branch using for the new scraper
+        """
+        if not branch_name:
+            branch_name = self.name.replace(" ", "-").lower()
+        try:
+            # Create a new branch
+            subprocess.run(["git", "checkout", "-b", branch_name], check=True)
+
+            print(f"Branch '{branch_name}' created and checked out successfully.")
+            return branch_name
+        except subprocess.CalledProcessError as e:
+            print(f"Error creating branch: {e}")
+
+    @staticmethod
+    def checkout_previous_branch():
+        try:
+            # Switch back to the previous branch
+            subprocess.run(["git", "checkout", "-"], check=True)
+
+            print("Switched back to the previous branch.")
+        except subprocess.CalledProcessError as e:
+            print(f"Error switching back to the previous branch: {e}")
+
+    def add_commit_push(self, file_path, branch_name, commit_message=None):
+        if not commit_message:
+            commit_message = f"{self.name} scraper is ready!"
+        try:
+            subprocess.run(["git", "add", file_path], check=True)
+
+            subprocess.run(["git", "commit", "-m", commit_message], check=True)
+
+            subprocess.run(["git", "push", "--set-upstream", "origin", branch_name], check=True)
+
+            print("Changes added, committed, and pushed successfully.")
+        except subprocess.CalledProcessError as e:
+            print(f"Error: {e}")
---
+++ Scraping tools/Outsource tools/UrlsHandler.py
@@ -0,0 +1,60 @@
+from googlesearch import search
+import pandas as pd
+from pathlib import Path
+
+
+class URLsHandler:
+    allowed_extensions = {'csv': ".csv", 'excel': ".xlsx"}
+
+    def initialize_company_dict(self, companies):
+        if isinstance(companies, list):
+            return {comp: "" for comp in companies}
+        if isinstance(companies, str):
+            result_dict = dict()
+            file = Path(companies)
+            if file.exists() and file.is_file and file.suffix in self.allowed_extensions.values():
+                df = pd.read_excel(companies) if companies.endswith('.xlsx') else pd.read_csv(companies)
+            for index, row in df.iterrows():
+                name = row.iloc[0]  # Assuming the name is in the first column
+                value = row.iloc[1] if len(row) > 1 else ''
+                result_dict[name] = value
+            return result_dict
+
+    def get_google_result(self, company, outsource):
+        try:
+            search_results = search(f'{outsource} {company} career', num=5, stop=1, pause=2)  # Add a pause of 2 seconds
+            for result_url in next(search_results):
+                if self.check_url(result_url):
+                    return result_url
+            return None
+        except StopIteration:
+            print("No search results found.")
+            return None
+        except Exception as e:
+            print(e)
+            return None
+
+    def check_url(self, name, url):
+        pass
+
+    def update_existing_urls(self, output_file_path, companies_dict):
+        try:
+            # Convert the dictionary to a DataFrame
+            df = pd.DataFrame(list(companies_dict.items()), columns=['Company', 'URL'])
+            # Write the DataFrame to the specified file path
+            if output_file_path.endswith(self.allowed_extensions['csv']):
+                df.to_csv(output_file_path, index=False)
+            elif output_file_path.endswith(self.allowed_extensions['excel']):
+                df.to_excel(output_file_path, index=False, engine='openpyxl')
+            else:
+                print("Unsupported file format. Please provide a CSV or Excel file.")
+            print(f"Data successfully written to {output_file_path}")
+        except Exception as e:
+            print(f"Error writing to file: {e}")
+
+    def create_new_branch(self, name, url):
+        pass
+
+    def scrape_with_dict(self, comp_dict):
+        for name, url in comp_dict.items():
+            self.create_new_branch(name, url)
