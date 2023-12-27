8cc64fc74af78aedfeccdbb95e0f93174a1b547a
ShaniShurkin <shanishurkin@gmail.com>
2023-12-27 14:32:06 +0200

    Pax8 scraper is ready!

---
+++ ScrapingTools/Scrapers/CompanyScrapers/ComeetScrapers/Pax8Scraper.py
@@ -0,0 +1,12 @@
+
+    from ScrapingTools.OutsourceTools.Comeet.BaseScraper import ComeetScraper
+    
+
+    class Pax8Scraper(ComeetScraper):
+        url = "https://www.comeet.com/jobs/pax8/52.008"
+        name = "Pax8"
+
+        def scrape(self):
+            super().scrape()
+
+    
