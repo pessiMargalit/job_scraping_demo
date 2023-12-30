d0b85683aa2df040e8df2d9d942840d7368bacd7
pessiMargalit <pessimargalit@gmail.com>
2023-12-30 21:45:59 +0200

    Gong.io scraper is ready!

---
+++ Scrapers/CompanyScrapers/GreenhouseScrapers/GongIoScraper.py
@@ -0,0 +1,11 @@
+
+from ScrapingTools.OutsourceTools.Greenhouse.GreenhouseScraper import GreenhouseScraper
+
+
+class GongioScraper(GreenhouseScraper): 
+    url = "https://boards.greenhouse.io/gongio"
+    name = 'Gong.io'
+
+    def scrape(self):
+        super().scrape()
+
