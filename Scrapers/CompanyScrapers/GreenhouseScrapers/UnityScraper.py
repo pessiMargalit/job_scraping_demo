5e9a6c7088958e355f51e5156551444c4ed5f7ce
pessiMargalit <pessimargalit@gmail.com>
2023-12-30 21:30:33 +0200

    Unity scraper is ready!

---
+++ Scrapers/CompanyScrapers/GreenhouseScrapers/UnityScraper.py
@@ -0,0 +1,11 @@
+
+from ScrapingTools.OutsourceTools.Greenhouse.GreenhouseScraper import GreenhouseScraper
+
+
+class UnityScraper(GreenhouseScraper): 
+    url = "https://boards.greenhouse.io/embed/job_board?for=unity3d"
+    name = 'Unity'
+
+    def scrape(self):
+        super().scrape()
+
