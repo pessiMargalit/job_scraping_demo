3148ba8e541d68a8304676d614461da0c332a498
pessiMargalit <pessimargalit@gmail.com>
2023-12-30 21:46:14 +0200

    Maven Clinic scraper is ready!

---
+++ Scrapers/CompanyScrapers/GreenhouseScrapers/MavenClinicScraper.py
@@ -0,0 +1,11 @@
+
+from ScrapingTools.OutsourceTools.Greenhouse.GreenhouseScraper import GreenhouseScraper
+
+
+class MavenclinicScraper(GreenhouseScraper): 
+    url = "https://boards.greenhouse.io/mavenclinic"
+    name = 'Maven Clinic'
+
+    def scrape(self):
+        super().scrape()
+
