04ef39667530bc001c5173a313ba3d9d15a82f96
pessiMargalit <pessimargalit@gmail.com>
2023-12-30 21:30:25 +0200

    Sisense scraper is ready!

---
+++ Scrapers/CompanyScrapers/GreenhouseScrapers/SisenseScraper.py
@@ -0,0 +1,11 @@
+
+from ScrapingTools.OutsourceTools.Greenhouse.GreenhouseScraper import GreenhouseScraper
+
+
+class SisenseScraper(GreenhouseScraper): 
+    url = "https://boards.greenhouse.io/embed/job_board?for=sisense"
+    name = 'Sisense'
+
+    def scrape(self):
+        super().scrape()
+
