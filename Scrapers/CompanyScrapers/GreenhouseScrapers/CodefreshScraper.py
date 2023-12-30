31ac777b2de63717979d612b7a4204fe48e9de00
pessiMargalit <pessimargalit@gmail.com>
2023-12-30 21:29:35 +0200

    Codefresh scraper is ready!

---
+++ Scrapers/CompanyScrapers/GreenhouseScrapers/CodefreshScraper.py
@@ -0,0 +1,11 @@
+
+from ScrapingTools.OutsourceTools.Greenhouse.GreenhouseScraper import GreenhouseScraper
+
+
+class CodefreshScraper(GreenhouseScraper): 
+    url = "https://boards.greenhouse.io/codefresh"
+    name = 'Codefresh'
+
+    def scrape(self):
+        super().scrape()
+
