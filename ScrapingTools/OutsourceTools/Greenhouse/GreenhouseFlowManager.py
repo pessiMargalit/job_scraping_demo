f0c0d0a1bb073b10744cb4d78c086bb810d8f652
pessiMargalit <pessimargalit@gmail.com>
2023-12-30 21:39:00 +0200

    Delete unnecessary files

--- ScrapingTools/OutsourceTools/Greenhouse/GreenhouseFlowManager.py
+++ ScrapingTools/OutsourceTools/Greenhouse/GreenhouseFlowManager.py
@@ -4,7 +4,7 @@ from ScrapingTools.OutsourceTools.Greenhouse.GreenhouseURLsHandler import Greenh
 if __name__ == "__main__":
     cuh = GreenhouseURLsHandler()
     # companies_dict = cuh.perform_urls_handler_flow()
-    companies_dict = cuh.initialize_company_dict(r"C:\Users\User\Downloads\1.xlsx")
+    companies_dict = cuh.initialize_company_dict('../../../Careers files/greenhouse_new.xlsx')
     # cc_general = GreenhouseCompany("")
     # cc_general.checkout_branch(cc_general.branch_name)
     for name, url in companies_dict.items():
