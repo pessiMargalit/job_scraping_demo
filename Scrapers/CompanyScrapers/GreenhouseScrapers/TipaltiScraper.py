1042dac2ed9e7fd29e9c9716b16b3ad884f28b28
pessiMargalit <pessimargalit@gmail.com>
2023-12-30 21:30:31 +0200

    Tipalti scraper is ready!

---
+++ Scrapers/CompanyScrapers/GreenhouseScrapers/TipaltiScraper.py
@@ -0,0 +1,11 @@
+
+from ScrapingTools.OutsourceTools.Greenhouse.GreenhouseScraper import GreenhouseScraper
+
+
+class TipaltiScraper(GreenhouseScraper): 
+    url = "https://boards.greenhouse.io/embed/job_board?for=tipaltisolutions&b=https%3A%2F%2Ftipalti.com%2Fcareers%2F"
+    name = 'Tipalti'
+
+    def scrape(self):
+        super().scrape()
+
