2682694c860f6810c2e7fc7a68efd5323950d739
YehuditShrg <y0548562311@outlook.co.il>
2023-12-31 14:01:19 +0200

    WEEL scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/WeelScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class WeelScraper(ComeetScraper):
+    url = "https://jobs.lever.co/weel/4461927f-e608-4fc4-93cb-e9b975382a96"
+    name = "WEEL"
+
+    def scrape(self):
+        super().scrape()
+
+    
