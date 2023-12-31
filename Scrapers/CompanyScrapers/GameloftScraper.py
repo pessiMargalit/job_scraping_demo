8ee117dc8b126c78582a36fb0073d9fa07e0362e
YehuditShrg <y0548562311@outlook.co.il>
2023-12-31 13:17:27 +0200

    Limestone Digital

--- Scrapers/CompanyScrapers/GameloftScraper.py
+++ Scrapers/CompanyScrapers/GameloftScraper.py
@@ -3,18 +3,21 @@ from Scrapers.Scraper import *
 
 class GameloftScraper(Scraper):
     name = "Gameloft"
-    url = 'https://www.malamteam.com/%d7%aa%d7%95%d7%a6%d7%90%d7%95%d7%aa-%d7%97%d7%99%d7%a4%d7%95%d7%a9-%d7%9e%d7%a9%d7%a8%d7%95%d7%aa/?pr=&a=&t='
+    url = 'https://careers.smartrecruiters.com/Gameloft'
 
 
     def scrape(self):
-            soup = self.scraping_unit(f"{self.url}?pr=&a={area}&t=")
-            careers = soup.find_all('div', class_='search-result-row item-row')
+            soup = self.scraping_unit(self.url)
+            careers = soup.find_all('li', class_='opening-job job column wide-7of16 medium-1of2')
             for job in careers:
-                title = job.findNext('div', class_='job-name').text.strip()
-                link = job.findNext('div', class_='item-col col-btn').find('a')['href']
-                location = areas[area]
+                title = job.findNext('h4', class_='details-title job-title link--block-target').text.strip()
+                link = job.findNext('a', class_='link--block details')['href']
+                # location = areas[area]
                 self.positions.append(self.Position(
                     title=title,
                     link=link,
-                    location=location
+                    # location=location
                 ))
+
+
+GameloftScraper().check_self()
