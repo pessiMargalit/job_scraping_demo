ecf14f7f30f2904967151fa79725f942cd7807bd
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:16:00 +0200

    Deloitte scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/DeloitteScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class DeloitteScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/deloitte/F7.00B"
+    name = "Deloitte"
+
+    def scrape(self):
+        super().scrape()
+
+    
