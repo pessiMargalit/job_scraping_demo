177e39db8296f528d148e65111760c3d21a33fac
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:13:37 +0200

    Custor scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/CustorScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class CustorScraper(ComeetScraper):
+    url = "nan"
+    name = "Custor"
+
+    def scrape(self):
+        super().scrape()
+
+    
