76718c1ba91ac16b539dba2f0e2876208007b72f
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:43:22 +0200

    morphisec scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/MorphisecScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class MorphisecScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/morphisec/65.00C"
+    name = "morphisec"
+
+    def scrape(self):
+        super().scrape()
+
+    
