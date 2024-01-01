f5a25cee7650e86dbfbbbee0c30eb917f51f149f
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:42:55 +0200

    joinattil scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/JoinattilScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class JoinattilScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/joinattil/38.00A"
+    name = "joinattil"
+
+    def scrape(self):
+        super().scrape()
+
+    
