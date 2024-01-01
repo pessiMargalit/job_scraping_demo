6df8eab641b34255a8516c2d9d9ec6a210b85409
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:18:04 +0200

    sodastream scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/SodastreamScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class SodastreamScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/sodastream/40.008"
+    name = "sodastream"
+
+    def scrape(self):
+        super().scrape()
+
+    
