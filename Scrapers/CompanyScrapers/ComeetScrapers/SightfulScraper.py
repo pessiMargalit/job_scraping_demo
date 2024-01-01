b689016286d9d054c77fdf64065fdbe3c7609bd0
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:15:24 +0200

    Sightful scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/SightfulScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class SightfulScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/sightful/26.00D"
+    name = "Sightful"
+
+    def scrape(self):
+        super().scrape()
+
+    
