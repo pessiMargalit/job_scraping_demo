155d6ae0263e13043b6c71484d27fd4f907e8690
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:14:35 +0200

    Kaltura scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/KalturaScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class KalturaScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/kaltura/E2.00D"
+    name = "Kaltura"
+
+    def scrape(self):
+        super().scrape()
+
+    
