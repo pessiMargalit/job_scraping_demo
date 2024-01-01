1b4e4fae8b0ca94bf4cfa6bc208c2c086ac854bf
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:44:09 +0200

    Aidoc scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/AidocScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class AidocScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/Aidoc/B4.007"
+    name = "Aidoc"
+
+    def scrape(self):
+        super().scrape()
+
+    
