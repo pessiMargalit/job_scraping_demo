a439c5e0fa06eddd5b3f9b2812dac5d8b1e5a6c8
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:14:58 +0200

    Pepperi scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/PepperiScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class PepperiScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/pepperi/72.007"
+    name = "Pepperi"
+
+    def scrape(self):
+        super().scrape()
+
+    
