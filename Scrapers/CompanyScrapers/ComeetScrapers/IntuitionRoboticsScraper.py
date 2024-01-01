a9236c8e6bf3b5618b3cf74e21888a812b929d31
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:41:53 +0200

    Intuition Robotics scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/IntuitionRoboticsScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class IntuitionroboticsScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/intuitionrobotics/B3.00B"
+    name = "Intuition Robotics"
+
+    def scrape(self):
+        super().scrape()
+
+    
