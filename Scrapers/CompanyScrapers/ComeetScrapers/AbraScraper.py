fbbcce52491d359398233a115afdbc76e59e475b
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:40:29 +0200

    Abra scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/AbraScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class AbraScraper(ComeetScraper):
+    url = "nan"
+    name = "Abra"
+
+    def scrape(self):
+        super().scrape()
+
+    
