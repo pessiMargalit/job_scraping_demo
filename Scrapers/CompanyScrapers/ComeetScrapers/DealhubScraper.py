2db1a42a6d4eae50036c05a873b9b2e2f1a84093
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:15:48 +0200

    Dealhub scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/DealhubScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class DealhubScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/dealhub/86.005"
+    name = "Dealhub"
+
+    def scrape(self):
+        super().scrape()
+
+    
