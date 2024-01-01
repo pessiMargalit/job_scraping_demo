7863caeb1b19724752f1053800d2034e58c97a53
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:16:31 +0200

    hibob scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/HibobScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class HibobScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/hibob/12.00A"
+    name = "hibob"
+
+    def scrape(self):
+        super().scrape()
+
+    
