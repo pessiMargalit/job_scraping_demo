6d95095f6ffb21e05df987d61d53688292adee0c
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:42:01 +0200

    Play Studios scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/PlayStudiosScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class PlaystudiosScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/playstudios/E2.00B"
+    name = "Play Studios"
+
+    def scrape(self):
+        super().scrape()
+
+    
