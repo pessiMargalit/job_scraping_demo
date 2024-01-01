263aefe951b86c6bb8c047a668ad7e089d6f267a
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:44:04 +0200

    Infidant scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/InfidantScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class InfidantScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/infinidat/D6.003"
+    name = "Infidant"
+
+    def scrape(self):
+        super().scrape()
+
+    
