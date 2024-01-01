bd4f5d09ff626b17b9cd2faf94b14bbfc8f62d11
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:14:51 +0200

    Signals Analytics scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/SignalsAnalyticsScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class SignalsanalyticsScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/signalsanalytics/51.002"
+    name = "Signals Analytics"
+
+    def scrape(self):
+        super().scrape()
+
+    
