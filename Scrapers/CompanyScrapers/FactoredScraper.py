06dbd33d6242939777a784176651d36a4a5b4291
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 12:59:25 +0200

    Factored scraper is done

---
+++ Scrapers/CompanyScrapers/FactoredScraper.py
@@ -0,0 +1,25 @@
+from Scrapers.Scraper import *
+
+
+class FactoredScraper(Scraper):
+    name = "Factored"
+    url = 'https://jobs.lever.co/factored'
+
+    def scrape(self):
+        soup = self.scraping_unit(self.url)
+        careers = soup.find_all('div', class_='posting')
+        for job in careers:
+            link = job.findNext('a', class_="posting-title")['href']
+            title = job.findNext('h5').text.strip()
+            location = job.findNext('span',
+                                    class_="display-inline-block small-category-label workplaceTypes").text.strip()
+            location += job.findNext('span',
+                                     class_="sort-by-location posting-category small-category-label location").text.strip()
+
+            self.positions.append(self.Position(
+                title=title,
+                link=link,
+                location=location
+            ))
+
+
