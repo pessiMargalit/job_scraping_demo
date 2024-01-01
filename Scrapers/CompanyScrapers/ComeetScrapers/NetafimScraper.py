9d15ebc3e4cfc1ea44e97b2556582e1ea7c35a2a
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:42:38 +0200

    netafim scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/NetafimScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class NetafimScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/netafim/B7.002"
+    name = "netafim"
+
+    def scrape(self):
+        super().scrape()
+
+    
