3c4880f54250be0d58a0b54087d514ca12d74907
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:41:45 +0200

    Checkmarx scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/CheckmarxScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class CheckmarxScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/checkmarx/C0.008"
+    name = "Checkmarx"
+
+    def scrape(self):
+        super().scrape()
+
+    
