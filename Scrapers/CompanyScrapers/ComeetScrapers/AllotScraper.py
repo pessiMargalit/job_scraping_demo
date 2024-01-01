1f9cbf4a475b24e2114ac55c60762d2b41f15b19
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:44:51 +0200

    allot scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/AllotScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class AllotScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/allot/C4.009"
+    name = "allot"
+
+    def scrape(self):
+        super().scrape()
+
+    
