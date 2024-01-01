6ece62179413ea7a375c41ab5fabdce426cb81da
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:17:17 +0200

    PayEm scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/PayemScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class PayemScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/payem/17.007"
+    name = "PayEm"
+
+    def scrape(self):
+        super().scrape()
+
+    
