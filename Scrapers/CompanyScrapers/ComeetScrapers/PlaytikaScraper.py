b2c2ce3c072dd79d2e5277e5fee817b9280394f3
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:16:55 +0200

    playtika scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/PlaytikaScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class PlaytikaScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/playtika/01.001"
+    name = "playtika"
+
+    def scrape(self):
+        super().scrape()
+
+    
