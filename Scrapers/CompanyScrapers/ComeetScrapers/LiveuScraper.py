12811ee6c4275e06aa770938f154cf26d1cde34c
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:43:34 +0200

    liveu scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/LiveuScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class LiveuScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/liveu/90.00C"
+    name = "liveu"
+
+    def scrape(self):
+        super().scrape()
+
+    
