c6eb49629dd5702282e53f1193710fd7d1bf7f60
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:42:44 +0200

    fiverr scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/FiverrScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class FiverrScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/fiverr/60.002"
+    name = "fiverr"
+
+    def scrape(self):
+        super().scrape()
+
+    
