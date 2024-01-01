a6521d36649d8a58d72bf4a1ce795858c3154317
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:40:45 +0200

    Ofakim scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/OfakimScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class OfakimScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/ofakim/85.00C"
+    name = "Ofakim"
+
+    def scrape(self):
+        super().scrape()
+
+    
