2f06aaf0f9d83ed9665863582d42af6576dce8a7
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:44:34 +0200

    aero nautics scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/AeroNauticsScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class AeronauticsScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/aeronautics/72.002"
+    name = "aero nautics"
+
+    def scrape(self):
+        super().scrape()
+
+    
