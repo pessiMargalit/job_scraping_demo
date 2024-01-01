edae33ada83e651e8defdd6bb2f717ede4f78c86
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:13:29 +0200

    Iron Source scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/IronSourceScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class IronsourceScraper(ComeetScraper):
+    url = "https://careers.unity.com/location/tel-aviv"
+    name = "Iron Source"
+
+    def scrape(self):
+        super().scrape()
+
+    
