09253478a33e951a5f2b049e20ec01647cac0bd4
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:15:42 +0200

    Comm-IT scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/CommItScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class CommitScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/comm-it/76.008"
+    name = "Comm-IT"
+
+    def scrape(self):
+        super().scrape()
+
+    
