1451aea316c5701b92df9d903f2eacb142c3dda7
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:14:18 +0200

    Bizzabo scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/BizzaboScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class BizzaboScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/bizzabo/A5.000"
+    name = "Bizzabo"
+
+    def scrape(self):
+        super().scrape()
+
+    
