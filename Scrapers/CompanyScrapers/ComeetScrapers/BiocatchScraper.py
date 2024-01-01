e6bc10042e5d6bf4d247fd9bb2df4f14c77044c5
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:16:50 +0200

    biocatch scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/BiocatchScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class BiocatchScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/biocatch/03.00E"
+    name = "biocatch"
+
+    def scrape(self):
+        super().scrape()
+
+    
