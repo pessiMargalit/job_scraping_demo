77c18ca2aaf780b8e815230410ad5224cd3b20bd
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:18:24 +0200

    Anyclip scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/AnyclipScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class AnyclipScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/anyclip/91.00D"
+    name = "Anyclip"
+
+    def scrape(self):
+        super().scrape()
+
+    
