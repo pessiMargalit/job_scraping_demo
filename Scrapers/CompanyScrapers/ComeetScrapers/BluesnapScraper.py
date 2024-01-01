9a790ff32b63a68ecfb39ec31c4451f33bf463ed
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:17:20 +0200

    bluesnap scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/BluesnapScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class BluesnapScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/bluesnap/73.002"
+    name = "bluesnap"
+
+    def scrape(self):
+        super().scrape()
+
+    
