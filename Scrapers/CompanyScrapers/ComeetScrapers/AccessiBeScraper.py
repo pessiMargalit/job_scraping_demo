f7e3206811452e133397ea5015ee1b008074cb1d
YehuditShrg <y0548562311@outlook.co.il>
2024-01-01 13:44:40 +0200

    accessi be scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/AccessiBeScraper.py
@@ -0,0 +1,12 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class AccessibeScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/accessibe/D5.00B/"
+    name = "accessi be"
+
+    def scrape(self):
+        super().scrape()
+
+    
