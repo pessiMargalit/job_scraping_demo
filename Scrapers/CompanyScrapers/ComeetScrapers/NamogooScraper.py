04ab50af4dad6ca145f32d7844757f8ea97a9e33
YehuditShrg <y0548562311@outlook.co.il>
2024-01-04 13:17:36 +0200

    Namogoo scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/NamogooScraper.py
@@ -0,0 +1,9 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class NamogooScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/namogoo/B4.006"
+    name = "Namogoo"
+
+    
