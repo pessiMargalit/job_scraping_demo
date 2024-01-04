633bc2423dcd724a1f2537d6345be23a67ff216f
YehuditShrg <y0548562311@outlook.co.il>
2024-01-04 13:17:36 +0200

    Dealhub scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/DealhubScraper.py
@@ -0,0 +1,9 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class DealhubScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/dealhub/86.005"
+    name = "Dealhub"
+
+    
