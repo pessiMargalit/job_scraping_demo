0132aec5964d2a31d695f24feb6d76123d247493
YehuditShrg <y0548562311@outlook.co.il>
2024-01-04 13:17:35 +0200

    Jifiti scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/JifitiScraper.py
@@ -0,0 +1,9 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class JifitiScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/jifiti/47.005"
+    name = "Jifiti"
+
+    
