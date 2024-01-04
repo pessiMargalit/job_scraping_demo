a8549537beae563d9413324567977172806261b3
YehuditShrg <y0548562311@outlook.co.il>
2024-01-04 13:17:29 +0200

    Vidac Pharma scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/VidacPharmaScraper.py
@@ -0,0 +1,9 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class VidacpharmaScraper(ComeetScraper):
+    url = "https://vidacpharma.com/en/"
+    name = "Vidac Pharma"
+
+    
