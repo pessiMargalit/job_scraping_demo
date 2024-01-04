d9f14358d181154ac7100e96b9a42d7927e20a20
YehuditShrg <y0548562311@outlook.co.il>
2024-01-04 13:09:02 +0200

    MyHeritage scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/MyheritageScraper.py
@@ -0,0 +1,9 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class MyheritageScraper(ComeetScraper):
+    url = "https://careers.myheritage.com/"
+    name = "MyHeritage"
+
+    
