9aea0effbdf5e6b924e9c567774e3c7b7e08edb0
YehuditShrg <y0548562311@outlook.co.il>
2024-01-04 13:16:58 +0200

    Fixes

--- Scrapers/CompanyScrapers/ComeetScrapers/NextsiliconScraper.py
+++
@@ -1,9 +0,0 @@
-
-from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
-
-
-class NextsiliconScraper(ComeetScraper):
-    url = "https://www.nextsilicon.com/careers/"
-    name = "NextSilicon"
-
-    
