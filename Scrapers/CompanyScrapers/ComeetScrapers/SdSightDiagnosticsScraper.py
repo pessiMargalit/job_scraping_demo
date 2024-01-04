6c52fdf913eaebb1ee1ce958c073a37d9b1f28a2
YehuditShrg <y0548562311@outlook.co.il>
2024-01-04 12:42:40 +0200

    SD Sight Diagnostics scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/SdSightDiagnosticsScraper.py
@@ -0,0 +1,9 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class SdsightdiagnosticsScraper(ComeetScraper):
+    url = "nan"
+    name = "SD Sight Diagnostics"
+
+    
