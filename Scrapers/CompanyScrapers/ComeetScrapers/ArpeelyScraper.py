341b4c2b21544c26821412090d1d0814730922d0
YehuditShrg <y0548562311@outlook.co.il>
2024-01-04 13:17:32 +0200

    Arpeely scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/ArpeelyScraper.py
@@ -0,0 +1,9 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class ArpeelyScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/arpeely/57.001"
+    name = "Arpeely"
+
+    
