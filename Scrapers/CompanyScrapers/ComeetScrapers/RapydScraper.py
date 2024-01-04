87cc32e5924b434f22a8dbe43604307a7d22e11a
YehuditShrg <y0548562311@outlook.co.il>
2024-01-04 13:17:35 +0200

    Rapyd scraper is ready!

---
+++ Scrapers/CompanyScrapers/ComeetScrapers/RapydScraper.py
@@ -0,0 +1,9 @@
+
+from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
+
+
+class RapydScraper(ComeetScraper):
+    url = "https://www.comeet.com/jobs/rapyd/73.00E"
+    name = "Rapyd"
+
+    
