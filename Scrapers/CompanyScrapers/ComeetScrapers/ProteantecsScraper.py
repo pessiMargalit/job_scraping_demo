0fc7fa47082d772af71a18fcf3c061c4ab62a977
YehuditShrg <y0548562311@outlook.co.il>
2024-01-04 12:45:42 +0200

    proteanTecs scraper is ready!

--- Scrapers/CompanyScrapers/ComeetScrapers/ProteantecsScraper.py
+++ Scrapers/CompanyScrapers/ComeetScrapers/ProteantecsScraper.py
@@ -4,6 +4,6 @@ from ScrapingTools.OutsourceTools.Comeet.ComeetScraper import ComeetScraper
 
 class ProteantecsScraper(ComeetScraper):
     url = "nan"
-    name = "Proteantecs"
+    name = "proteanTecs"
 
     
