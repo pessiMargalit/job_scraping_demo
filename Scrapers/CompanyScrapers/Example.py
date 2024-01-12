1158500999cd0678571732d253cfa4db8fd11d00
snirsh <s.sharristh@gmail.com>
2024-01-12 17:50:08 +0200

    remove scrapers

--- Scrapers/CompanyScrapers/Example.py
+++
@@ -1,9 +0,0 @@
-from Scrapers.Scraper import Scraper
-
-
-class PoopScraper(Scraper):
-    name = 'Poop'
-    url = 'example.com'
-
-    def scrape(self):
-        pass
--- Scrapers/CompanyScrapers/PoopScraper.py
+++
@@ -1,9 +0,0 @@
-from Scrapers.Scraper import Scraper
-
-
-class ExampleScraper(Scraper):
-    name = 'Example'
-    url = 'example.com'
-
-    def scrape(self):
-        pass
