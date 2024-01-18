a2e64654d46f76cbe2fa6843e36603613a0b190b
YehuditShrg <y0548562311@outlook.co.il>
2024-01-18 13:04:08 +0200

    fixed file names

--- Scrapers/CompanyScrapers/HydroxScraper.py
+++ Scrapers/CompanyScrapers/HydroxScraper.py
@@ -1,15 +1,16 @@
-from Scrapers.Scraper import Scraper
+from Scrapers.Scraper import *
 
-
+# we have to take the location
 class HydroxScraper(Scraper):
-    name = 'Hydrox'
+    name = 'hydrox'
     url = 'https://hydrox.earth/hydro-x-careers/'
+    location = 'west Jerusalem'  # default location, when not set its automatically 'Jerusalem'
 
     def scrape(self):
         soup = self.scraping_unit(self.url)
         for div_tag in soup.findAll('div', {'class': 'elementor-post__text'}):
-            title = div_tag.findNext('h3', {'class': 'elementor-post__title'})
-            link = div_tag.findNext('a')['href']
+            title = div_tag.find_next('h3')
+            link = div_tag.find_next('a')['href']
             self.positions.append(self.Position(
                 title=title.text,
                 link=link
--- Scrapers/running_the_test.py
+++ Scrapers/running_the_test.py
@@ -1,28 +1,46 @@
-import os
 import subprocess
+
+import os
+import glob
 
 
 def run_pytest_on_files(directory):
-    """
-    This function iterates over all files in the specified directory.
-    For each file, it runs the command 'pytest --companies <file_name>'
-    and prints the filename along with whether the test passed or failed.
-    """
-    for filename in os.listdir(directory):
-        if filename.endswith('.py') and not filename.__contains__('__init__'):
-            # Construct the pytest command
-            command = f"pytest --companies {filename}"
-            # Run the command and capture the output and return code
-            result = subprocess.run(command, shell=True, cwd=directory, capture_output=True, text=True)
-            print(result)
-            # Check if the pytest command was successful (return code 0)
-            if result.returncode == 0:
-                status = 'passed'
-            else:
-                status = 'failed'
-            print(f"{filename}: {status}")
+    # List all Python files in the directory, excluding __init__.py
+    filenames = [filename for filename in os.listdir(directory)
+                 if filename.endswith('.py') and '__init__' not in filename]
+
+    # Construct the pytest command
+    command = f"pytest --companies {','.join(filenames)}"
+    print(command)
+    # Execute the command in the specified directory
+    result = subprocess.run(args=command, shell=True, cwd=directory, capture_output=True, text=True)
+
+    # Print the result (stdout and stderr)
+    print("STDOUT:", result.stdout)
+    print("STDERR:", result.stderr)
+    print("Return Code:", result.returncode)
 
 
-# Example usage
-directory_path = r"C:\Users\user\Documents\תכנות\Frashboard\job_scraping_demo\Scrapers\CompanyScrapers"
-run_pytest_on_files(directory_path)
+def is_valid_file_name(path):
+    """
+    prints the wrong file names
+    """
+    # Loop through all .py files in the specified directory
+    for file_path in glob.glob(os.path.join(directory_path, '*.py')):
+        # Extract the file name from the path
+        file_name = os.path.basename(file_path)
+
+        # Check if 'init' is not in the file name and 'Scraper' is not in the file name
+        if 'init' not in file_name.lower() and 'Scraper' not in file_name:
+            print(file_name)
+            # Construct new file name by adding 'Scraper' to the original name
+            # new_file_name = file_name.strip(".py")
+            # new_file_name += 'Scraper.py'
+            # new_file_path = os.path.join(directory_path, new_file_name)
+            # os.rename(file_path, new_file_path)
+            # print(f'Renamed "{file_name}" to "{new_file_name}"')
+
+
+directory_path = r'C:\Users\user\Documents\תכנות\Frashboard\job_scraping_demo\Scrapers\CompanyScrapers'
+# run_pytest_on_files(directory_path)
+# is_valid_file_name(directory_path)
