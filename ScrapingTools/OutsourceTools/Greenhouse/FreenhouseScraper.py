b6fdf59d9bcbf0d9e6b5fd39a705ba53e3e09cc1
ChanyRosental <chanyrosental@gmail.com>
2024-01-23 14:16:43 +0200

    add GreenhouseScraper

---
+++ ScrapingTools/OutsourceTools/Greenhouse/FreenhouseScraper.py
@@ -0,0 +1,20 @@
+from Scrapers.Scraper import *
+
+
+class GreenhouseScraper(Scraper):
+    base_url = 'https://boards.greenhouse.io/'
+    url = 'https://boards.greenhouse.io/'
+    name = 'Greenhouse'
+
+
+    def scrape(self):
+        soup = self.scraping_unit(self.url)
+        for position_div in soup.findAll('div', {'class': 'opening'}):
+            title = position_div.findNext('a')
+            location = position_div.findNext('span', {'class': 'location'})
+            link = title['href'] if title else None
+            self.positions.append(self.Position(
+                title=title.text if title else None,
+                link=f'{self.base_url}{link}',
+                location=location.text if location else None
+            ))
