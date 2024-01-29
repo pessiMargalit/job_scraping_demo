b835aefe64900185b56ec1b0ea7a1f7c3fd57cc1
pessiMargalit <pessimargalit@gmail.com>
2024-01-29 21:21:06 +0200

    Larsen And Toubro Scraper  with passed tests.

--- Scrapers/CompanyScrapers/LarsenAndToubroScraper.py
+++ Scrapers/CompanyScrapers/LarsenAndToubroScraper.py
@@ -8,31 +8,8 @@ class LarsenAndToubroScraper(Scraper):
     url = 'https://larsentoubrocareers.peoplestrong.com/job/joblist'
     location = 'Jerusalem'
 
-    def find_requests_url(self, my_format):
-        caps = DesiredCapabilities.CHROME
-        caps['goog:loggingPrefs'] = {'performance': 'ALL'}
-        driver = Scraper.selenium_url_maker(self.url)
-        logs = driver.get_log("performance")
-        events = [json.loads(log['message'])['message'] for log in logs]
-        get_request_ids = []
-        for event in events:
-            if event['method'] == 'Network.requestWillBeSent':
-                if 'request' in event['params']:
-                    if event['params']['request']['method'] == 'GET' or event['params']['request']['method'] == 'POST':
-                        get_request_ids.append(event['params']['requestId'])
-        for event in events:
-            if event['method'] == 'Network.responseReceived':
-                if 'response' in event['params']:
-                    response = event['params']['response']
-                    if event['params']['requestId'] in get_request_ids:
-                        if my_format in response['mimeType'] and 'jobs' in response['url']:
-                            return response['url']
-
-        driver.quit()
-
-    #
     def scrape(self):
-        jobs_json_url = self.find_requests_url(my_format='json')
+        jobs_json_url = 'https://larsentoubrocareers.peoplestrong.com/api/cp/rest/altone/cp/jobs/v1?offset=0&limit=45'
         for i in range(0, 945, 45):
             jobs_json_url.replace(f'offset={i - 45}', f'offset={i}')
             all_jobs_response = requests.get(jobs_json_url)
@@ -47,3 +24,4 @@ class LarsenAndToubroScraper(Scraper):
                         link=link.strip() if link else self.url,
                         location=location.strip() if location else None,
                     ))
+
