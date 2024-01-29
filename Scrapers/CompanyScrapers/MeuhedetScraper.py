515e5dffa1fb7a33afbfad08cec44a81b2471daf
Shoshana <shanishurkin@gmail.com>
2024-01-29 22:54:42 +0200

    Remove Unnecessaries headers

--- Scrapers/CompanyScrapers/MeuhedetScraper.py
+++ Scrapers/CompanyScrapers/MeuhedetScraper.py
@@ -6,7 +6,7 @@ class MeuhedetScraper(Scraper):
     name = 'מאוחדת'
     base_url = "https://www.meuhedet.co.il/"
     url = f'{base_url}search?mod=400'
-    location = 'Jerusalem'
+    location = 'ירושלים'
     pages_num = None
 
     def __init__(self):
@@ -16,27 +16,6 @@ class MeuhedetScraper(Scraper):
 
     def get_careers_json(self, page_number):
         url = "https://hcpgnf9t5w-1.algolianet.com/1/indexes/*/queries"
-
-        headers = {
-            "Accept": "*/*",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "he-IL,he;q=0.9,en-US;q=0.8,en;q=0.7",
-            "Cache-Control": "no-cache",
-            "Connection": "keep-alive",
-            "Content-Length": "324",
-            "Host": "hcpgnf9t5w-1.algolianet.com",
-            "Origin": "https://www.meuhedet.co.il",
-            "Pragma": "no-cache",
-            "Referer": "https://www.meuhedet.co.il/",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "cross-site",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-            "Content-Type": "application/x-www-form-urlencoded",
-            "sec-ch-ua": '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Windows"'
-        }
 
         query_params = {
             "x-algolia-agent": "Algolia for JavaScript (4.14.2); Browser (lite)",
@@ -56,7 +35,7 @@ class MeuhedetScraper(Scraper):
         }
 
         # Send the POST request
-        response = requests.post(url, headers=headers, params=query_params, json=payload)
+        response = requests.post(url, params=query_params, json=payload)
 
         if response.status_code == 200:
             try:
@@ -78,7 +57,7 @@ class MeuhedetScraper(Scraper):
             job_id = job["Id"]
             link = f"{self.base_url}jobs/{job_id}/"
             self.positions.append(self.Position(
-                title=title,
+                title=title.strip(),
                 location=location,
                 link=link
             ))
@@ -87,3 +66,6 @@ class MeuhedetScraper(Scraper):
         with concurrent.futures.ThreadPoolExecutor() as executor:
             pages = range(1, int(self.pages_num / 20) + 1)
             executor.map(self.scrape_page, pages)
+
+
+MeuhedetScraper().check_self()
