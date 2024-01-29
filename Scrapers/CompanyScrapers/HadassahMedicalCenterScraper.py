bfede991ad38ea7b46d3cd70874774f3f8b5afa5
ChanyRosental <chanyrosental@gmail.com>
2024-01-29 15:21:37 +0200

    Hadassah scraper

--- Scrapers/CompanyScrapers/HadassahMedicalCenterScraper.py
+++ Scrapers/CompanyScrapers/HadassahMedicalCenterScraper.py
@@ -1,21 +1,27 @@
 from urllib.parse import urljoin
+from concurrent.futures import ThreadPoolExecutor
 from Scrapers.Scraper import *
 
 
 class HadassahMedicalCenterScraper(Scraper):
     name = 'Hadassah Medical Center'
     url = 'https://www.hadassah.org.il/careers/'
-    # location = 'Jerusalem'
+
+    def process_job(self, tag_li):
+        title = tag_li.find('h2', {'class': 'general-container_title'}).text
+        link = tag_li.find('a')['href']
+        return self.Position(
+            title=title.strip(),
+            link=urljoin(self.url, link),
+            location=self.location
+        )
 
     def scrape(self):
         soup = self.scraping_unit(self.url)
         list_jobs = soup.find('ul', {'class': 'general-container-wide_list'})
-        for tag_li in list_jobs.findAll('li'):
-            title = tag_li.find('h2', {'class': 'general-container_title'}).text
-            link = tag_li.find('a')['href']
-            self.positions.append(self.Position(
-                title=title.strip(),
-                link=urljoin(self.url, link),
-                location=self.location
-            ))
-HadassahMedicalCenterScraper().check_self()
+
+        with ThreadPoolExecutor() as executor:
+            futures = [executor.submit(self.process_job, tag_li) for tag_li in list_jobs.findAll('li')]
+            for future in futures:
+                result = future.result()
+                self.positions.append(result)
