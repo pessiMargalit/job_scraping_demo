7dca4186072550bccba6982e8c21e5c43eb4a6fc
pessiMargalit <pessimargalit@gmail.com>
2024-01-30 11:42:32 +0200

    EisnerAmper Scraper with parallel code

--- Scrapers/CompanyScrapers/EisnerAmperScraper.py
+++ Scrapers/CompanyScrapers/EisnerAmperScraper.py
@@ -1,3 +1,5 @@
+from concurrent.futures import ThreadPoolExecutor
+
 from Scrapers.Scraper import *
 
 
@@ -13,21 +15,26 @@ class EisnerAmperScraper(Scraper):
         num = num_pages[-2].text
         return int(num)
 
+    def scrape_page(self, page):
+        soup = self.scraping_unit(f'{self.url}?page={page}#header')
+        positions = soup.find_all(attrs={'class': "card card-job"})
+        for pos in positions:
+            title = pos.findNext('h2', {'class': "card-title"})
+            link = pos.findNext('a', {"class": "stretched-link js-view-job"})
+            metadata = pos.findAllNext('li', {"class": "list-inline-item"})
+            content = metadata[0]
+            location = metadata[1]
+            self.positions.append(self.Position(
+                title=title.text.strip() if title else None,
+                link=f'{self.base_url}{link["href"]}' if link else self.url,
+                location=location.text.strip() if location else self.location,
+                content=content.text.strip() if content else None
+            ))
+
     def scrape(self):
         num_pages = self.calculate_num_of_career_pages() + 1
-        for i in range(1, num_pages):
-            soup = self.scraping_unit(f'{self.url}?page={i}#header')
-            positions = soup.find_all(
-                attrs={'class': "card card-job"})
-            for pos in positions:
-                title = pos.findNext('h2', {'class': "card-title"})
-                link = pos.findNext('a', {"class": "stretched-link js-view-job"})
-                metadata = pos.findAllNext('li', {"class": "list-inline-item"})
-                content = metadata[0]
-                location = metadata[1]
-                self.positions.append(self.Position(
-                    title=title.text.strip() if title else None,
-                    link=f'{self.base_url}{link["href"]}' if link else self.url,
-                    location=location.text.strip() if location else self.location,
-                    content=content.text.strip() if content else None
-                ))
+        with ThreadPoolExecutor() as executor:
+            futures = [executor.submit(self.scrape_page, i) for i in range(1, num_pages)]
+            # Wait for all futures to complete
+            for future in futures:
+                future.result()
