1858fba34eca9ba61980e3b1470ff9edd94eaab2
pessiMargalit <pessimargalit@gmail.com>
2024-01-30 14:37:54 +0200

    Efi scraping is not allowed.

--- Scrapers/CompanyScrapers/EfiScraper.py
+++ Scrapers/CompanyScrapers/EfiScraper.py
@@ -5,9 +5,9 @@ from Scrapers.Scraper import *
 
 class EfiScraper(Scraper):
     name = 'Efi'
-    # url = 'https://phe.tbe.taleo.net/phe03/ats/careers/v2/searchResults?next&rowFrom={}' \
-    #       '&act=search&sortColumn=null&sortOrder=null&currentTime=1706608095791'
-    url = 'https://www.efi.com/about-efi/careers/job-search/'
+    url = 'https://phe.tbe.taleo.net/phe03/ats/careers/v2/searchResults?next&rowFrom={}' \
+          '&act=search&sortColumn=null&sortOrder=null&currentTime=1706608095791'
+    # url = 'https://www.efi.com/about-efi/careers/job-search/'
     location = 'Jerusalem'
 
     # def scrape_page(self):
@@ -20,27 +20,41 @@ class EfiScraper(Scraper):
     #     positions = soup.find_all(attrs={'class': "oracletaleocwsv2-accordion-head-info"})
     #     for pos in positions:
     #         print(pos)
-        #     title = pos.findNext('h4', {'class': "oracletaleocwsv2-head-title"})
-        #     link = pos.findNext('a', {"class": "viewJobLink"})
-        #     location = pos.findNext('div')
-            # self.positions.append(self.Position(
-            #     title=title.text.strip() if title else None,
-            #     link=link["href"] if link else self.url,
-            #     location=location.text.strip() if location else self.location,
-            # ))
+    #     title = pos.findNext('h4', {'class': "oracletaleocwsv2-head-title"})
+    #     link = pos.findNext('a', {"class": "viewJobLink"})
+    #     location = pos.findNext('div')
+    # self.positions.append(self.Position(
+    #     title=title.text.strip() if title else None,
+    #     link=link["href"] if link else self.url,
+    #     location=location.text.strip() if location else self.location,
+    # ))
 
+    # def scrape_page(self, page):
+    #     soup = self.scraping_unit(self.url.format(page))
+    #     positions = soup.find_all(attrs={'class': "oracletaleocwsv2-accordion-head-info"})
+    #     for pos in positions:
+    #         title = pos.findNext('h4', {'class': "oracletaleocwsv2-head-title"})
+    #         link = pos.findNext('a', {"class": "viewJobLink"})
+    #         location = pos.findNext('div')
+    #         self.positions.append(self.Position(
+    #             title=title.text.strip() if title else None,
+    #             link=link["href"] if link else self.url,
+    #             location=location.text.strip() if location else self.location,
+    #         ))
     def scrape_page(self, page):
-        soup = self.scraping_unit(self.url.format(page))
-        positions = soup.find_all(attrs={'class': "oracletaleocwsv2-accordion-head-info"})
-        for pos in positions:
-            title = pos.findNext('h4', {'class': "oracletaleocwsv2-head-title"})
-            link = pos.findNext('a', {"class": "viewJobLink"})
-            location = pos.findNext('div')
-            self.positions.append(self.Position(
-                title=title.text.strip() if title else None,
-                link=link["href"] if link else self.url,
-                location=location.text.strip() if location else self.location,
-            ))
+        all_jobs_response = requests.get(self.url.format(page))
+        if all_jobs_response.status_code == 200:
+            all_jobs_data = all_jobs_response.json()
+            for job in all_jobs_data['response']:
+                print(job)
+                title = job.findNext('h4', {'class': "oracletaleocwsv2-head-title"})
+                link = job.findNext('a', {"class": "viewJobLink"})
+                location = job.findNext('div')
+                self.positions.append(self.Position(
+                    title=title.text.strip() if title else None,
+                    link=link["href"] if link else self.url,
+                    location=location.text.strip() if location else self.location,
+                ))
 
     def scrape(self):
         num_pages = 50
