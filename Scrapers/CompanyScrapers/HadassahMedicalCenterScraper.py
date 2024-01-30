8170195c020428e43c27abb23e0f441d04fbfe5c
ChanyRosental <chanyrosental@gmail.com>
2024-01-30 13:03:36 +0200

    add headers

--- Scrapers/CompanyScrapers/HadassahMedicalCenterScraper.py
+++ Scrapers/CompanyScrapers/HadassahMedicalCenterScraper.py
@@ -12,7 +12,10 @@ class HadassahMedicalCenterScraper(Scraper):
     def scrape(self):
         # soup = self.scraping_unit(self.url)
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                    "Accept-Language": "en-US,en;q=0.5",
+                    "Referer": "https://www.google.com/"}
         req = Request(self.url, headers=headers)
         page = urlopen(req)
         soup = BeautifulSoup(page, 'html.parser')
