6d9726c71685b8f54fa341c66b9c9fc33a4561d8
ChanyRosental <chanyrosental@gmail.com>
2024-01-31 12:29:58 +0200

    Shaare Zedek scraper

--- Scrapers/CompanyScrapers/ShaareZedekScraper.py
+++ Scrapers/CompanyScrapers/ShaareZedekScraper.py
@@ -1,4 +1,4 @@
-from urllib.parse import urljoin
+from urllib.parse import urljoin, quote
 from Scrapers.Scraper import *
 
 
@@ -11,11 +11,13 @@ class ShaareZedekScraper(Scraper):
         soup = self.scraping_unit(self.url)
         for job in soup.findAll("div", {"class": "hot-job"}):
             title = job.findNext("h5", {"class": "hot-job-name"})
-            link = urljoin(self.url,'/פרטי-משרה/?jobcode=JB-2234')
+            path = '/פרטי-משרה/'
+            encoded_path = quote(path, safe='/')
+            job_code = job.findNext("div", {"class": "info"}).get('job-code')
+            query = '?jobcode='+job_code
+            link = urljoin(self.url, encoded_path + query)
             self.positions.append(self.Position(
                 title=title.text,
                 location=self.location,
                 link=link
             ))
-
-ShaareZedekScraper().check_self()
