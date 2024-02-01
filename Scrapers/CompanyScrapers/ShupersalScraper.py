1c366bfcdb0e1630ef7ee32a01b12b9c6cb114fd
YehuditShrg <y0548562311@outlook.co.il>
2024-02-01 15:24:05 +0200

    probebly the problem is selenium

--- Scrapers/CompanyScrapers/ShupersalScraper.py
+++ Scrapers/CompanyScrapers/ShupersalScraper.py
@@ -8,32 +8,30 @@ class ShupersalScraper(Scraper):
     url = "https://career.shufersal.co.il/site/SearchWithParams"
 
     def scrape(self):
-        res = requests.get(self.url)
-        if res.status_code != 200:
-            raise "###########"
-    # driver = self.selenium_url_maker(self.url)
-    # load_more = WebDriverWait(driver, 10) \
-    #     .until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a#load_more_btn')))
-    #
-    # soup = BeautifulSoup(driver.page_source, 'html.parser')
-    # total = int(soup.find("span", {'ng-bind': 'TotalPublishedPositions'}).text.strip())
-    #
-    # for i in range(0, int(total / 5)):
-    #     load_more.click()
-    #
-    # html = driver.page_source
-    # soup = BeautifulSoup(html, 'html.parser')
-    # jobs = soup.findAll("div", class_="item ng-scope")
-    # for job in jobs:
-    #     title = job.findNext("div", class_="job ng-binding").text.strip()
-    #     location = job.findNext("div", class_="location ng-binding").text.strip()
-    #     link = urljoin(self.url, job.findNext("a", class_="btn")["href"])
-    #
-    #     self.positions.append(
-    #         self.Position(
-    #             title=title,
-    #             location=location,
-    #             link=link
-    #         )
-    #     )
-    # driver.quit()
+        driver = self.selenium_url_maker(self.url)
+        load_more = WebDriverWait(driver, 10) \
+            .until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a#load_more_btn')))
+
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        total = int(soup.find("span", {'ng-bind': 'TotalPublishedPositions'}).text.strip())
+
+        for i in range(0, int(total / 5)):
+            load_more.click()
+
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        jobs = soup.findAll("div", class_="item ng-scope")
+        for job in jobs:
+            title = job.findNext("div", class_="job ng-binding").text.strip()
+            location = job.findNext("div", class_="location ng-binding").text.strip()
+            link = urljoin(self.url, job.findNext("a", class_="btn")["href"])
+
+            self.positions.append(
+                self.Position(
+                    title=title,
+                    location=location,
+                    link=link
+                )
+            )
+        driver.quit()
+
