76c5d9bdbb554a8ad56ddbd3b2febe0485bfefcf
ShaniShurkin <shanishurkin@gmail.com>
2024-02-05 14:14:14 +0200

    Gros & Co Scraper passed all tests

--- Scrapers/CompanyScrapers/GrossAndCoScraper.py
+++ Scrapers/CompanyScrapers/GrossAndCoScraper.py
@@ -6,19 +6,21 @@ TIMEOUT_IN_SECONDS = 10
 class GrossAndCoScraper(Scraper):
     name = 'Gross & Co'
     url = 'https://www.gkh-law.com/career/'
+    location = "ירושלים"
 
-    def scrape_unit(self, url):
+    def get_positions_lst(self, url):
         soup = self.scraping_unit(url)
         positions = soup.findAll("button", {"class": "accordion accordion_2"})
+        return positions
+
+    def scrape(self):
+        positions = self.get_positions_lst(self.url)
+        positions.extend(self.get_positions_lst('https://www.gkh-law.com/he/career/'))
         for pos in positions:
             title = pos
             self.positions.append(self.Position(
                 title=title.text.strip() if title else None,
             ))
-
-    def scrape(self):
-        self.scrape_unit(self.url)
-        self.scrape_unit('https://www.gkh-law.com/he/career/')
 
 
 GrossAndCoScraper().check_self()
