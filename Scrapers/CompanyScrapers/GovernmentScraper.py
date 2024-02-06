22a43b9fa61054ff1381e0316ccd968be4d12dff
YehuditShrg <y0548562311@outlook.co.il>
2024-02-06 14:29:33 +0200

    parellel code

--- Scrapers/CompanyScrapers/GovernmentScraper.py
+++ Scrapers/CompanyScrapers/GovernmentScraper.py
@@ -1,8 +1,9 @@
+import concurrent.futures
 from urllib.parse import urljoin
 
 from Scrapers.Scraper import *
 
-NUM_OF_JOBS_IN_JSON = 10
+NUM_OF_JOBS_IN_JSON = 50
 
 
 class GovernmentScraper(Scraper):
@@ -12,49 +13,59 @@ class GovernmentScraper(Scraper):
     def get_jobs(self, url):
         are_open_positions = False
         res = requests.get(url)
+        if not res.ok:
+            return True
         result = json.loads(res.content)["results"]
         for job in result:
-            last_date = job["LastDate"]
-            given_date = datetime.strptime(last_date, "%Y-%m-%dT%H:%M:%SZ")
-            current_date = datetime.utcnow()
-            if given_date < current_date:
-                continue
-            title = job["Title"]
-            link = urljoin("https://www.gov.il/he/departments/publications/drushim/", job["UrlName"])
-            res = requests.get(link)
-            if not res.ok:
-                # it's mata data , not a position
-                continue
-            if job["OfficeDesc"]:
-                company = job["OfficeDesc"][0]
-            else:
-                company = None
-            title = job["Title"]
-            if job["CitiesDesc"]:
-                location = job["CitiesDesc"][0]
-            else:
-                location = None
-            self.positions.append(
-                self.Position(
-                    title=title.strip(),
-                    link=link,
-                    location=location,
-                    company=company if company else self.name
+            try:
+                last_date = job["LastDate"]
+                given_date = datetime.strptime(last_date, "%Y-%m-%dT%H:%M:%SZ")
+                current_date = datetime.utcnow()
+                if given_date < current_date:
+                    continue
+                link = urljoin("https://www.gov.il/he/departments/publications/drushim/", job["UrlName"])
+                res = requests.get(link)
+                if not res.ok:
+                    # it's mata data , not a position
+                    continue
+                if job["OfficeDesc"]:
+                    company = job["OfficeDesc"][0]
+                else:
+                    company = None
+                title = job["Title"]
+                if job["CitiesDesc"]:
+                    location = job["CitiesDesc"][0]
+                else:
+                    location = None
+                self.positions.append(
+                    self.Position(
+                        title=title.strip(),
+                        link=link,
+                        location=location,
+                        company=company if company else self.name
+                    )
                 )
-            )
-            are_open_positions = True
+                are_open_positions = True
+            except:
+                continue
         return are_open_positions
 
     @staticmethod
     def get_num_of_total_jobs():
-        res = requests.get("https://www.gov.il/he/api/PublicationApi/Index?limit=10&skip=0")
+        res = requests.get(f"https://www.gov.il/he/api/PublicationApi/Index?limit={NUM_OF_JOBS_IN_JSON}&skip=0")
         return json.loads(res.content)["total"]
 
     def scrape(self):
-        num_of_total_jobs = GovernmentScraper().get_num_of_total_jobs()
-        for index in range(50, num_of_total_jobs, NUM_OF_JOBS_IN_JSON):
-            flag = self.get_jobs(f"https://www.gov.il/he/api/PublicationApi/Index?limit={index}&skip={index - 50}")
-            if not flag:
-                # its means that was page with no any open position
-                return
+        num_of_total_jobs = self.get_num_of_total_jobs()
 
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = []
+            for index in range(NUM_OF_JOBS_IN_JSON, num_of_total_jobs, NUM_OF_JOBS_IN_JSON):
+                json_url = f"https://www.gov.il/he/api/PublicationApi/Index?limit={index}&skip={index - NUM_OF_JOBS_IN_JSON}"
+                futures.append(executor.submit(self.get_jobs, json_url))
+
+            for future in concurrent.futures.as_completed(futures):
+                future.result()
+
+
+GovernmentScraper().check_self()
