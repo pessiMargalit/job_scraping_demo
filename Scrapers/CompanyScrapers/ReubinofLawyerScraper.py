d8a2e385912ab39b29bc4c9b799c37b5bc992348
pessiMargalit <pessimargalit@gmail.com>
2024-02-08 11:11:04 +0200

    Reubinof Lawyer scraper with passed tests.

--- Scrapers/CompanyScrapers/ReubinofLawyerScraper.py
+++ Scrapers/CompanyScrapers/ReubinofLawyerScraper.py
@@ -1,16 +1,22 @@
+import re
+
 from Scrapers.Scraper import Scraper
 
 
 class ReubinofLawyerScraper(Scraper):
     name = "גבריאל ראובינוף - משרד עו''ד"
     url = "https://reubinof.co.il/%d7%a2%d7%91%d7%95%d7%93%d7%94-%d7%91%d7%a8%d7%90%d7%95%d7%91%d7%99%d7%a0%d7%95%d7%a3/"
+    location = 'ירושלים'
 
     def scrape(self):
         soup = self.scraping_unit(self.url)
-        positions = soup.find("div", class_="elementor-element elementor-element-1ae74ae elementor-widget elementor-widget-text-editor")
-# "elementor-element elementor-element-1ae74ae elementor-widget elementor-widget-text-editor"
-# "elementor-element elementor-element-baa9ee5 elementor-widget elementor-widget-text-editor"
+        pattern = r'elementor-element elementor-element-[a-zA-Z0-9]+ elementor-widget elementor-widget-text-editor'
+        positions = soup.find_all('div', class_=re.compile(pattern))
 
-        for pos in positions:
-            title = pos.text.strip()
-            print(title)
+        for pos in positions[0:-3:2]:
+            title = pos.findNext('div', {'class': 'elementor-widget-container'})
+            link = pos.findNext('a', {'class': 'elementor-button-link elementor-button elementor-size-sm'})
+            self.positions.append(self.Position(
+                title=title.text.strip(),
+                link=link['href'] if link else None,
+            ))
